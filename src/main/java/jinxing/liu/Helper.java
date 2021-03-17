b16e863bcc631197a941545d3aa673ea4ca6cd8d
jinxing.liu <821901008@qq.com>
2021-03-17 16:58:15 +0800

    init: 初始化

---
+++ src/main/java/jinxing/liu/Helper.java
@@ -0,0 +1,28 @@
+package jinxing.liu;
+
+import java.util.Map;
+
+public class Helper {
+    public static String getQueryString(String url, Map<String, Object> query) {
+        StringBuilder builder = new StringBuilder(url);
+        if (!query.isEmpty()) {
+            // ?
+            if (url.contains("?")) {
+                // username=1&
+                if (!url.endsWith("&")) {
+                    builder.append("&");
+                }
+            } else {
+                builder.append("?");
+            }
+
+            for (Map.Entry<String, Object> entry : query.entrySet()) {
+                builder.append(entry.getKey()).append("=").append(entry.getValue().toString()).append("&");
+            }
+
+            builder.delete(builder.length() - 1, builder.length());
+        }
+
+        return builder.toString();
+    }
+}
---
+++ src/test/java/jinxing/liu/HelperTest.java
@@ -0,0 +1,31 @@
+package jinxing.liu;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HelperTest {
+
+    @Test
+    public void test() {
+        Map<String, Object> query = new HashMap<>();
+        String url = Helper.getQueryString("http://www.baidu.com", query);
+        System.out.println(url);
+        Assert.assertEquals("http://www.baidu.com", url);
+
+        query.put("username", "jinxing.liu");
+        query.put("age", 25);
+        query.put("name", "hello");
+
+        url = Helper.getQueryString("http://www.baidu.com", query);
+        System.out.println(url);
+
+        Assert.assertEquals("http://www.baidu.com?name=hello&age=25&username=jinxing.liu", url);
+
+        url = Helper.getQueryString("http://www.baidu.com?s=123&", query);
+        System.out.println(url);
+        Assert.assertEquals("http://www.baidu.com?s=123&name=hello&age=25&username=jinxing.liu", url);
+    }
+}
